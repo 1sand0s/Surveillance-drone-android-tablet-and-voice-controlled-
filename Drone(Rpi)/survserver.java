739a64a0f31bef7df6d2108929a10bcba475d3cb
1sand0s <1sand0s@users.noreply.github.com>
2015-06-28 18:47:22 +0530

    Update survserver.java

--- Drone(Rpi)/survserver.java
+++ Drone(Rpi)/survserver.java
@@ -2,12 +2,32 @@ import java.net.*;
 import java.util.*;
 import java.io.*;
 
-public class survserver
+public class Rpi_Server_Java
 {
-   
-    static Vector cliSoc;
-    static Vector Log;
-    static 
+    Rpi_Server_Java() throws Exception
+    {
+    	ServerSocket soc=new ServerSocket(9000,100);
+        System.out.println("Waiting for connection .....");
+        while(true)
+        {    
+            Socket CSoc=soc.accept();
+            new AcceptClient(CSoc);
+        }
+    }
+    public static void main(String args[]) throws Exception
+    {
+        new Rpi_Server_Java();
+    }
+}
+
+class AcceptClient extends Thread
+{
+    Socket ClientSocket;
+    BufferedReader br;
+    BufferedWriter pr;
+    static ArrayList<Socket> cliSoc;
+    static ArrayList<String>Log;
+    /*static 
     {
 	System.loadLibrary("cprogstart");
 	System.loadLibrary("cprogsearch");
@@ -23,40 +43,22 @@ public class survserver
     public static native void back();
     public static native void left();  
     public static native void right();
-    public static native void cam();
-    survserver() throws Exception
-    {
-        ServerSocket soc=new ServerSocket(9000,100);
-        cliSoc=new Vector();
-        Log=new Vector();
-        System.out.println("Waiting for connection .....");
-        while(true)
-        {    
-            Socket CSoc=soc.accept();
-            AcceptClient obClient=new AcceptClient(CSoc);
-        }
-    }
-    public static void main(String args[]) throws Exception
-    {
-        
-       survserver  ob=new survserver();
-    }
-
-class AcceptClient extends Thread
-{
-    Socket ClientSocket;
-    BufferedReader br;
-    PrintWriter pr;
+    public static native void cam();*/
+    
     AcceptClient (Socket CSoc) throws Exception
     {
+    	
+        cliSoc=new ArrayList<Socket>();
+        Log=new ArrayList<String>();
         ClientSocket=CSoc;
 
         br=new BufferedReader(new InputStreamReader(ClientSocket.getInputStream()));
-        pr=new PrintWriter(new OutputStreamWriter(ClientSocket.getOutputStream()),true);
+        pr=new BufferedWriter(new OutputStreamWriter(ClientSocket.getOutputStream()));
         
         String LoginName=br.readLine();
-	pr.println("Connected ");
-	pr.flush();
+        pr.write("Connected");
+        pr.newLine();
+        pr.flush();
         System.out.println("User Logged In :" + LoginName);
         Log.add(LoginName);
         cliSoc.add(ClientSocket);    
@@ -73,66 +75,74 @@ class AcceptClient extends Thread
                 String msgFromClient=new String();
                 msgFromClient=br.readLine();
 				String f=msgFromClient.substring(msgFromClient.lastIndexOf(' ')+1,msgFromClient.length());
+				System.out.println(f);
 				f.trim();
 				if(f.equalsIgnoreCase("start"))
 				{
-						pr.println("Starting");
+						pr.write("Starting");
+						pr.newLine();
 						pr.flush();
-						start1();
+						//start1();
 		         	}
 				else if(f.startsWith("search"))
 				{
 						
-						pr.println("Searching");
+						pr.write("Searching");
+						pr.newLine();
 						pr.flush();
 						f=f+" ";
 						String g=f.substring(f.indexOf("-"),f.length()-1);
-						search(g);
+						//search(g);
 				}
 				else if(f.equalsIgnoreCase("front"))
 				{
-						pr.println("Moving front");
-                                  		pr.flush();
-						front();
+						pr.write("Moving front");
+						pr.newLine();
+                        pr.flush();
+						//front();
 				}
 				else if(f.equalsIgnoreCase("back"))
 				{
-					pr.println("Moving back");
-					pr.flush();
-					back();
+						pr.write("Moving back");
+						pr.newLine();
+						pr.flush();
+						//back();
 				}
 				else if(f.equalsIgnoreCase("left"))
 				{
-					pr.println("Moving left");
-					pr.flush();
-					left();
+						pr.write("Moving left");
+						pr.newLine();
+						pr.flush();
+						//left();
 				}
 				else if(f.equalsIgnoreCase("right"))
 				{
-					pr.println("Moving Right");
-					pr.flush();
-					right();
+						pr.write("Moving Right");
+						pr.newLine();
+						pr.flush();
+						//right();
 				}
 				else if (f.equalsIgnoreCase("cam"))
 				{
-					pr.println("Initializing cam");
-					pr.flush();
-					cam();
+						pr.write("Initializing cam");
+						pr.newLine();
+						pr.flush();
+						//cam();
 				}
 
                 StringTokenizer st=new StringTokenizer(msgFromClient);
                 String Sendto=st.nextToken();                
                 String MsgType=st.nextToken();
-                int iCount=0;
+                int i=0;
                 
                 if(MsgType.equals("LOGOUT"))
                 {
-                    for(iCount=0;iCount<Log.size();iCount++)
+                    for(i=0;i<Log.size();i++)
                     {
-                        if(Log.elementAt(iCount).equals(Sendto))
+                        if(Log.get(i).equals(Sendto))
                         {
-                            Log.removeElementAt(iCount);
-                            cliSoc.removeElementAt(iCount);
+                            Log.remove(i);
+                            cliSoc.remove(i);
                             System.out.println("User " + Sendto +" Logged Out ...");
                             break;
                         }
@@ -146,14 +156,15 @@ class AcceptClient extends Thread
                     {
                         msg=msg+" " +st.nextToken();
                     }
-                    for(iCount=0;iCount<Log.size();iCount++)
+                    for(i=0;i<Log.size();i++)
                     {
-                        if(Log.elementAt(iCount).equals(Sendto))
+                        if(Log.get(i).equals(Sendto))
                         {    
-                            Socket tSoc=(Socket)cliSoc.elementAt(iCount);                            
-                            PrintWriter tdout=new PrintWriter(new OutputStreamWriter(tSoc.getOutputStream()),true);
-                            tdout.println(msg); 
-			    tdout.flush();
+                            Socket tSoc=(Socket)cliSoc.get(i);                            
+                            BufferedWriter tdout=new BufferedWriter(new OutputStreamWriter(tSoc.getOutputStream()));
+                            tdout.write(msg);
+                            tdout.newLine();
+                            tdout.flush();
                             break;
                         }
                     }
@@ -170,9 +181,7 @@ class AcceptClient extends Thread
                 ex.printStackTrace();
             }
             
-            
-            
         }        
     }
 }
-}
+
